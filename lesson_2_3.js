9d0c22c712dbf8b545d620e8c5d6b6eff0b9e1da
Roman <naydak@gmail.com>
2019-02-08 08:30:22 +0300

    сделал задание 3

--- lesson_2_3.js
+++ lesson_2_3.js
@@ -1,1 +1,13 @@
 "use strict";
+let a = 6;
+let b = 4;
+
+if ((a >= 0) && (b >= 0)) {
+  console.log(a - b);
+}
+else if ((a < 0) && (b < 0)) {
+  console.log(a * b);
+}
+else if (((a < 0) && (b >= 0)) || ((a >= 0) && (b < 0))) {
+  console.log(a + b);
+}
