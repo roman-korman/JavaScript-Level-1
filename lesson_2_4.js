8d29c0cc70ee4abd8f1b9d0577df28510a08b4db
Roman <naydak@gmail.com>
2019-02-08 08:47:44 +0300

    сделал задание 4

--- lesson_2_4.js
+++ lesson_2_4.js
@@ -1,1 +1,23 @@
 "use strict";
+function calcAddition(a, b) {
+  return a + b;
+}
+
+function calcSubtraction(a, b) {
+  return a - b;
+}
+
+function calcMultiplication(a, b) {
+  return a * b;
+}
+
+function calcDivision(a, b) {
+  return a / b;
+}
+let a = 4;
+let b = 3;
+
+console.log(calcAddition(a, b));
+console.log(calcSubtraction(a, b));
+console.log(calcMultiplication(a, b));
+console.log(calcDivision(a, b));
