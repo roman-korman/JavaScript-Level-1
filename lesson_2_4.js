39063f6fc115a6a1dafb9e572f4707f1e29151d7
Roman <naydak@gmail.com>
2019-02-09 00:56:21 +0300

    выполнил 6ое задание

--- lesson_2_4.js
+++ lesson_2_4.js
@@ -1,4 +1,12 @@
 "use strict";
+//объявление переменных
+let a = 0;
+let b = 0;
+
+//функция расчёта значений по следующему принципу:
+//если a и b положительные, вывести их разность;
+//если а и b отрицательные, вывести их произведение;
+//если а и b разных знаков, вывести их сумму;
 function calcAddition(a, b) {
   return a + b;
 }
@@ -14,10 +22,13 @@ function calcMultiplication(a, b) {
 function calcDivision(a, b) {
   return a / b;
 }
-let a = 4;
-let b = 3;
 
-console.log(calcAddition(a, b));
-console.log(calcSubtraction(a, b));
-console.log(calcMultiplication(a, b));
-console.log(calcDivision(a, b));
+//запрос переменных у пользователя
+a = +prompt('Введи первое число');
+b = +prompt('Введи второе число');
+
+//вывод данных пользователю
+alert(calcAddition(a, b));
+alert(calcSubtraction(a, b));
+alert(calcMultiplication(a, b));
+alert(calcDivision(a, b));
