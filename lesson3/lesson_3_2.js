c9a6908c07aae5574fbe86e0ea861bec046a52f5
Roman <naydak@gmail.com>
2019-02-12 19:04:47 +0300

    выполнил задание 2, проставил комментарии

--- lesson3/lesson_3_2.js
+++ lesson3/lesson_3_2.js
@@ -1,32 +1,32 @@
 "use strict";
-//объявляем переменные
-//id 0, goods 'monitor', bugrand: 'BenQ', price 23000.00, quantity 1
-//id 1, goods 'laptop', bugrand: 'Apple', price 150000.00, quantity 1
-//id 1, goods 'laptopYearWarranty', bugrand: 'Apple', price 15000.00, quantity 2
-let basket = [
-  [{
-      id: 0,
-      goods: 'monitor',
-      bugrand: 'BenQ',
-      price: 23000.00,
-      quantity: 1
-    },
-    {
-      id: 1,
-      goods: 'laptop',
-      bugrand: 'Apple',
-      price: 150000.00,
-      quantity: 1
-    },
-    {
-      id: 1,
-      goods: 'laptopYearWarranty',
-      bugrand: 'Apple',
-      price: 15000.00,
-      quantity: 2
-    }
-  ]
+//объявляю массив
+let basket = [{
+    id: 0, //идетификатор строки
+    goods: 'monitor', //группа товара
+    bugrand: 'BenQ', //бренд товара
+    price: 23000.00, //стоимость за единицу товара
+    quantity: 1 //количество товра в корзине
+  },
+  {
+    id: 1,
+    goods: 'laptop',
+    bugrand: 'Apple',
+    price: 150000.00,
+    quantity: 1
+  },
+  {
+    id: 1,
+    goods: 'laptopYearWarranty',
+    bugrand: 'Apple',
+    price: 15000.00,
+    quantity: 2
+  }
 ]
+//объявляю переменю для вывода суммы
+let sum = 0;
 
+for (let i = 0; i < basket.length; i++) { //определяем количество альтераций цикла
+  sum = sum + basket[i].price * basket[i].quantity; //на каждой альтерации прибавляем стоимость строки с товаром (цена на количество)
+}
 
-console.log(basket);
+console.log(sum) //вывод информации
