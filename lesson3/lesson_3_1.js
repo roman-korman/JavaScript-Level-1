7c109bc1f661e6870cba8ccebf98dc6f221f9b73
Roman <naydak@gmail.com>
2019-02-12 14:04:03 +0300

    сделал задания 1, 3,4

--- lesson3/lesson_3_1.js
+++ lesson3/lesson_3_1.js
@@ -1,2 +1,39 @@
 "use strict";
-//объявляем переменные
+//Дан массив
+let a = [
+  [{
+    name: 'Mary',
+    age: 23
+  }, {
+    name: 'Mike',
+    age: 45
+  }, {
+    name: 'Nick',
+    age: 11
+  }],
+  [{
+    name: 'Adam',
+    age: 56
+  }, {
+    name: 'Sara',
+    age: 21
+  }, {
+    name: 'Don',
+    age: 22
+  }],
+  [{
+    name: 'Karl',
+    age: 34
+  }, {
+    name: 'Marta',
+    age: 76
+  }, {
+    name: 'John',
+    age: 19
+  }]
+];
+
+//Скопируйте его в переменную b и выведите в консоль. .slice() без значений в скобках выполняет полное клонирование массива.
+let b = a.slice();
+//Вывод массива
+console.log(b);
--- lesson3/lesson_3_2.js
+++ lesson3/lesson_3_2.js
@@ -1,2 +1,32 @@
 "use strict";
 //объявляем переменные
+//id 0, goods 'monitor', bugrand: 'BenQ', price 23000.00, quantity 1
+//id 1, goods 'laptop', bugrand: 'Apple', price 150000.00, quantity 1
+//id 1, goods 'laptopYearWarranty', bugrand: 'Apple', price 15000.00, quantity 2
+let basket = [
+  [{
+      id: 0,
+      goods: 'monitor',
+      bugrand: 'BenQ',
+      price: 23000.00,
+      quantity: 1
+    },
+    {
+      id: 1,
+      goods: 'laptop',
+      bugrand: 'Apple',
+      price: 150000.00,
+      quantity: 1
+    },
+    {
+      id: 1,
+      goods: 'laptopYearWarranty',
+      bugrand: 'Apple',
+      price: 15000.00,
+      quantity: 2
+    }
+  ]
+]
+
+
+console.log(basket);
