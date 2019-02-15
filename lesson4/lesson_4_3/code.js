aae3d2b9fc0d408be854fa1ff618272eb0c2999a
Roman <naydak@gmail.com>
2019-02-15 09:47:47 +0300

    ДОработал механику игры
    1) добавил возможность двигаться по диаганали
    2) добавил ограничения по сторонам и углам карты. При попытке выпасть из карты игрок остаётся на месте
    3) добавил проверку вводимых символов

--- lesson4/lesson_4_3/code.js
+++ lesson4/lesson_4_3/code.js
@@ -1,25 +1,52 @@
 let game = {
 
-    run() {
-        while (true) {
-            let direction = prompt('Введите направление (2, 8, 4 или 6)');
+  run() {
+    while (true) {
+      let direction = prompt('Введите направление (1- вниз, влево / 2 - вниз / 3 - вниз, вправо / 4 - влево / 6 -вправо / 7 - вверх, влево / 8 - вверх / 9 - вверх, вправо)');
 
-            if (direction === null) {
-                console.log('Игра окончена');
-                return;
-            }
-
-            let nextPoint = mover.getNextPoint(direction);
-            renderer.clear();
-            player.move(nextPoint);
-            renderer.render();
-        }
-    },
-
-    init() {
-        console.log('Для начала игры введите команду game.run()');
+      if (direction === null) {
+        console.log('Игра окончена');
+        return;
+      } else if (direction != 1 && direction != 2 && direction != 3 && direction != 4 && direction != 6 && direction != 7 && direction != 8 && direction != 9) {
+        alert('Пропуск хода')
+      }
+      let nextPoint = mover.getNextPoint(direction);
+      if (nextPoint.x > 9 && nextPoint.y > 9) {
+        nextPoint = mover.getBackPoint(direction);
+        alert('Вы дошли до павого-нижнего угла этого мира')
+      } else if (nextPoint.x < 0 && nextPoint.y > 9) {
+        nextPoint = mover.getBackPoint(direction);
+        alert('Вы дошли до левого-нижнего угла этого мира')
+      } else if (nextPoint.x < 0 && nextPoint.y < 0) {
+        nextPoint = mover.getBackPoint(direction);
+        alert('Вы дошли до левого-верхнего угла этого мира')
+      } else if (nextPoint.x > 9 && nextPoint.y < 0) {
+        nextPoint = mover.getBackPoint(direction);
+        alert('Вы дошли до правого-верхнего угла этого мира')
+      } else if (nextPoint.x > 9) {
+        nextPoint = mover.getBackPoint(direction);
+        alert('Вы дошли до правой границы этого мира')
+      } else if (nextPoint.x < 0) {
+        nextPoint = mover.getBackPoint(direction);
+        alert('Вы дошли до левой границы этого мира')
+      } else if (nextPoint.y < 0) {
+        nextPoint = mover.getBackPoint(direction);
+        alert('Вы дошли до верхней границы этого мира')
+      } else if (nextPoint.y > 9) {
+        nextPoint = mover.getBackPoint(direction);
+        alert('Вы дошли до нижней границы этого мира')
+      } else {
+        renderer.clear();
+        player.move(nextPoint);
         renderer.render();
-    },
+      }
+    }
+  },
+
+  init() {
+    console.log('Для начала игры введите команду game.run()');
+    renderer.render();
+  },
 
 };
 
@@ -27,4 +54,3 @@ let game = {
 
 
 game.init();
-
