432f0eeaa7a1bd94d41914ff7a2a07ac651b2a54
Roman <naydak@gmail.com>
2019-02-17 01:18:17 +0300

    Выполнил второе задание
    Продолжить работу с интернет-магазином, а именно, перенести функционал
    подсчета корзины на объектно-ориентированную базу:
    • создать объект Basket с полем goodList и методами countTotalPrice и
    countTotalNumber (методы должны считать общую стоимость того, что в goodList
    и общее количество того, что в goodList)
    • создать объект Good c полями: name, price и методом putToBasket
    • после этого создать массив товаров (объектов Good), пройтись по нему в
    цикле и положить в корзину по одному товару на четной позиции и по 2 товара
    на нечетной позиции
    • вывести общую стоимость корзины и общее количество товаров в корзине

--- lesson4/lesson_4_1.js
+++ lesson4/lesson_4_1.js
@@ -7,20 +7,23 @@ let obj = {};
  *Функция запрашивает и анализирует вводимую информацию на корректность и запускает процесс пересчёта из цифрового значения в объекты
  */
 function didgitsAnalysis() {
-  let userDidgit = prompt('Введите число от 0 до 999');
+  let userDidgit = prompt("Введите число от 0 до 999");
   if (userDidgit === null) {
-    alert('Отмена операции');
+    alert("Отмена операции");
     return;
-  } else if (userDidgit == 0) { //дальнейшие функции не могут работать со значением 0, приходится выводить отдельную проверку
-    obj['единицы'] = 0;
+  } else if (userDidgit == 0) {
+    //дальнейшие функции не могут работать со значением 0, приходится выводить отдельную проверку
+    obj["единицы"] = 0;
     console.log(obj);
     return;
-  } else if (userDidgit > 0 && userDidgit <= 999) { //проверка на корректность ввода
+  } else if (userDidgit > 0 && userDidgit <= 999) {
+    //проверка на корректность ввода
     objectsFilling(splitDigits(userDidgit)); //запуск функций
     console.log(obj);
     return;
-  } else { //обработка исключений
-    alert('Введено не корректное значение')
+  } else {
+    //обработка исключений
+    alert("Введено не корректное значение");
   }
   didgitsAnalysis();
 }
@@ -42,18 +45,18 @@ function objectsFilling(massif) {
   for (let i = 0; i < massif.length; i++) {
     switch (i) {
       case 0:
-        obj['единицы'] = massif[0];
+        obj["единицы"] = massif[0];
       case 1:
         if (massif[1] == undefined) {
           break;
         } else {
-          obj['десятки'] = massif[1];
+          obj["десятки"] = massif[1];
         }
       case 2:
         if (massif[2] == undefined) {
           break;
         } else {
-          obj['сотни'] = massif[2];
+          obj["сотни"] = massif[2];
         }
     }
     return obj; //возврат заполненного объекта
