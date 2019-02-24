d9621ba18ac5b3dbfb9c065d08e471b5dda45ebe
Roman <naydak@gmail.com>
2019-02-24 14:29:31 +0300

    выполнил второе задание
    Создать форму в html со следующими полями:
    • Логин - текстовое поле
    • Пароль - поле типа password
    • Повтор пароля - поле типа password
    • Кнопка отправить форму
    Необходимо реализовать валидацию этой формы по следующим правилам:
    • Логин - должно содержать как минимум 1 символ, не более 50 символов.
    • Пароль - минимум 5 символов, максимум 50.
    • Повтор пароля - значение должно совпадать с полем пароль.
    • Кнопка отправить форму - при нажатии на кнопку форма должна провериться, при
    прохождении проверки, форма отправляется, если проверка не была пройдена, то:
    o Каждое поле, которое не прошло проверку должно выделяться красным
    цветом.
    o Под каждым полем, что не прошло проверку, должна писаться подсказка по
    какой причине проверка провалилась.
    Пользоваться аттрибутами HTML5 запрещено, необходимо реализовать проверки с
    помощью javascript. Можно пользоваться сторонними стилевыми библиотеками
    (bulma, bootstrap).

--- lesson6/6-2/js/script.js
+++ lesson6/6-2/js/script.js
@@ -1,46 +1,55 @@
 "use strict";
-let renderer = {
-  render() {
-    let table = this.generateTable();
-    document.body.insertAdjacentHTML('afterbegin', table);
-  },
-
-  generateTable() {
-    let board = '';
-    for (let y = 0; y <= config.rowsCount; y++) { //счётчик строк оставил из игры
-      if (y == config.rowsCount) { //определение последней строки
-        board += '<tr>';
-        for (let x = 0; x <= config.colsCount; x++) {
-          if (x == 0) { //отделение пустой ячейки
-            board += '<td class="contents"></td>';
-          } else { //вывод массива с буквами
-            board += '<td class="contents">' + letter[x - 1] + '</td>';
-          }
-        }
-        board += '</tr>';
-      } else {
-        board += '<tr>';
-        board += '<td class="contents">' + didgit[y] + '</td>'; //в каждом цикле сначала выводится ячейка с цифрой из массива
-        if (y % 2 == 1) { //определение чётности строк
-          /**
-           * за итерацию рисуется сразу и чёрная и белая ячейка. Учитывая то, что мы точно знаем формат выводимой таблицы не стал делать возможность вывода нечётного количества
-           */
-          for (let x = 0; x < config.colsCount; x += 2) {
-            board += '<td class="black"> </td>';
-            board += '<td class="white"> </td>';
-          }
-        } else {
-          for (let x = 0; x < config.colsCount; x += 2) { // за итерацию рисуется сразу и чёрная и белая ячейка
-            board += '<td class="white"> </td>';
-            board += '<td class="black"> </td>';
-          }
-        }
-        board += '</tr>';
-      }
-    }
-    return '<table><tbody>' + board + ' </tbody></table>';
-  },
-
-
-};
-renderer.render();
+/**
+ * Функция проверки поля пароля. Отрабатывает, когда с поля убирается фокус
+ */
+login.onblur = function() {
+  let errorMessage = document.getElementById('login_error');
+  let log = this.value;
+  if (log.length < 2) { //проверяю длину
+    errorMessage.innerHTML = 'Login is too short';
+    this.classList.add("error");
+  } else if (log.length > 50) { //проверяю длину
+    errorMessage.innerHTML = 'Login is too long';
+    this.classList.add("error");
+  } else { //снимаю ошибку
+    this.classList.remove("error");
+    errorMessage.innerHTML = null;
+  }
+}
+/**
+ * Проверка пароля на соответствие правилам
+ */
+password.onblur = function() {
+  let errorMessage = document.getElementById('password_error');
+  let pass = this.value;
+  if (pass.length < 6) { //проверяю длину
+    errorMessage.innerHTML = 'Password is too short';
+    this.classList.add("error");
+  } else if (pass.length > 50) { //проверяю длину
+    errorMessage.innerHTML = 'Login is too long';
+    this.classList.add("error");
+  } else { //снимаю ошибку
+    this.classList.remove("error");
+    errorMessage.innerHTML = null;
+  }
+}
+/**
+ * Проверка подверждения пароля на соответствие первому
+ */
+passwordCheck.onblur = function() {
+  let errorMessage = document.getElementById('passwordCheck_error');
+  let pass = this.value;
+  if (pass.length < 6) { //проверяю длину
+    errorMessage.innerHTML = 'Password is too short';
+    this.classList.add("error");
+  } else if (pass.length > 50) { //проверяю длину
+    errorMessage.innerHTML = 'Login is too long';
+    this.classList.add("error");
+  } else if (this.value != password.value) { //сравниваю два значения
+    errorMessage.innerHTML = 'Passwords have to coincide';
+    this.classList.add("error");
+  } else { //снимаю ошибку
+    this.classList.remove("error");
+    errorMessage.innerHTML = null;
+  }
+}
